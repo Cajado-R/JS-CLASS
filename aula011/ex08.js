547c8d4688f3728e40c48269c3e6228c17d92553
Cláudio Cajado <101744023+Cajado-R@users.noreply.github.com>
2023-01-23 18:52:26 -0300

    Initial Commit

---
+++ aula011/ex08.js
@@ -0,0 +1,7 @@
+var vel = 22.600
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel  >= 80){
+    console.log('Você ultrapassou a velocidade permitida, está multado')
+}else{
+    console.log('Tenha uma boa viagem')
+}
